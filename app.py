fa9cfad58fa704e7d3c0d5aaf0294d58ff78a06b
phanquan277 <phanquan277@devsecops.lab>
2026-01-17 16:49:05 +0000

    Fix 500 error: Restore DB_PASSWORD variable

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 AWS_ACCESS_KEY_ID = "AKIA5QY72W3V9L8M1Z4K" 
 AWS_SECRET_ACCESS_KEY = "z8a9s8d7f6g5h4j3k2l1m0n9b8v7c6x5z4a3s2d1"
 
+DB_PASSWORD = "admin_password_123"
+
 @app.route('/')
 def hello():
     return f"Hello DevSecOps TestV2 CI/CD Pipeline! Database password is: {DB_PASSWORD}"
